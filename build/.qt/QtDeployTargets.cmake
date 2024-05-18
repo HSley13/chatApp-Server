898d30972555fea0a7848c7b024f794d830c6f48
Sley HORTES <sleyhortes13@gmail.com>
2024-05-18 08:53:45 +0800

    Database lill prob

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_server_FILE /Users/test/Documents/client---server---client_chat_application/build/src/server/server.app/Contents/MacOS/server)
-set(__QT_DEPLOY_TARGET_client_FILE /Users/test/Documents/client---server---client_chat_application/build/src/clients/client.app/Contents/MacOS/client)
