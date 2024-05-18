d82d1af67364e93028ac02c2f55b19913d41b3ed
Sley HORTES <sleyhortes13@gmail.com>
2024-05-19 03:53:07 +0800

    Web Implementation DONE 2.2

---
+++ src/clients/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  ".rcc/qrc_client_images.cpp"
+  "CMakeFiles/client_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/client_autogen.dir/ParseCache.txt"
+  "client_autogen"
+  "CMakeFiles/client.dir/.rcc/qrc_client_images.cpp.o"
+  "CMakeFiles/client.dir/.rcc/qrc_client_images.cpp.o.d"
+  "CMakeFiles/client.dir/chat_line.cpp.o"
+  "CMakeFiles/client.dir/chat_line.cpp.o.d"
+  "CMakeFiles/client.dir/chat_protocol.cpp.o"
+  "CMakeFiles/client.dir/chat_protocol.cpp.o.d"
+  "CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/client.dir/client_chat_window.cpp.o"
+  "CMakeFiles/client.dir/client_chat_window.cpp.o.d"
+  "CMakeFiles/client.dir/client_main.cpp.o"
+  "CMakeFiles/client.dir/client_main.cpp.o.d"
+  "CMakeFiles/client.dir/client_main_window.cpp.o"
+  "CMakeFiles/client.dir/client_main_window.cpp.o.d"
+  "CMakeFiles/client.dir/client_manager.cpp.o"
+  "CMakeFiles/client.dir/client_manager.cpp.o.d"
+  "client.js"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
