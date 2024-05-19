b983c0ee45f1c91f92245370c3f8acc8ecd3a3c0
Sley HORTES <sleyhortes13@gmail.com>
2024-05-19 15:26:54 +0800

    Configuring path for Github Pages

---
+++ src/server/server_main_window.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "server_chat_window.h"
+#include "server_manager.h"
+
+class server_main_window : public QMainWindow
+{
+    Q_OBJECT
+public:
+    server_main_window(sql::Connection *db_connection, QWidget *parent = nullptr);
+    ~server_main_window();
+
+private:
+    sql::Connection *_db_connection;
+
+    QListWidget *_list;
+    QTabWidget *_tabs;
+
+    QPushButton *_disconnect_all;
+
+    static server_manager *_server;
+    static chat_protocol *_protocol;
+
+    QStatusBar *_status_bar;
+    static QHash<QString, QWidget *> _window_map;
+
+private slots:
+    void on_new_client_connected(QWebSocket *client);
+    void on_new_client_disconnected(QWebSocket *client);
+
+    void on_client_name_changed(QString original_name, QString old_name, QString name);
+
+    void on_is_typing_received(QString sender, QString receiver);
+
+    void close_tabs(int index);
+};
